07cbac9d1370d4a8e27aa9215cffbfed5fdde88a
yz18710109979 <79241484@qq.com>
2019-08-12 16:04:57 +0800

    x

--- hadoop-all/src/main/java/org/apache/hadoop/mapreduce/lib/input/FileInputFormat.java
+++ hadoop-all/src/main/java/org/apache/hadoop/mapreduce/lib/input/FileInputFormat.java
@@ -21,22 +21,21 @@ package org.apache.hadoop.mapreduce.lib.input;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
-import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.mapred.LocatedFileStatusFetcher;
-import org.apache.hadoop.mapred.SplitLocationInfo;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
--- hadoop-all/src/main/java/org/apache/hadoop/mapreduce/lib/partition/HashPartitioner.java
+++ hadoop-all/src/main/java/org/apache/hadoop/mapreduce/lib/partition/HashPartitioner.java
@@ -12,7 +12,20 @@ public class HashPartitioner<K, V> extends Partitioner<K, V> {
   /** Use {@link Object#hashCode()} to partition. */
   public int getPartition(K key, V value,
                           int numReduceTasks) {
-    return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+	  System.out.println(key.hashCode());
+	  String hashCode = Integer.toBinaryString(key.hashCode());
+	  System.out.println("二进制的hashCode：" + hashCode);
+	  System.out.println(Integer.MAX_VALUE);
+	  int maxValue = Integer.MAX_VALUE;
+	  String binaryString = Integer.toBinaryString(maxValue);
+	  System.out.println("二进制的int最大值：" + binaryString);
+	  return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+  }
+  
+  public static void main(String[] args) {
+	  HashPartitioner<String, String> hashPartitioner = new HashPartitioner<String, String>();
+	  int i = hashPartitioner.getPartition("123", "123", 11);
+	  System.out.println(i);
   }
 
 }
